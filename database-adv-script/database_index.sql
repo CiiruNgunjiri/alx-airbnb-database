70186703485de039c5a7303441b59db078e3f401
LindaNgunjiri <ciiru.ngunjiri@gmail.com>
2025-07-05 13:42:32 +0300

    Updated database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -19,3 +19,26 @@ CREATE INDEX IF NOT EXISTS idx_properties_price ON properties(price);
 -- Optional composite index for frequent combined queries on booking by user and date
 CREATE INDEX IF NOT EXISTS idx_booking_user_start_date ON booking(user_id, start_date);
 
+-- -------------------------------------------------------------------
+-- To measure query performance BEFORE creating indexes, run:
+
+-- EXPLAIN ANALYZE
+-- SELECT * FROM booking WHERE user_id = '9e9490ab-e46b-4565-ab3b-1231412051d8';
+
+-- -------------------------------------------------------------------
+-- To measure query performance AFTER creating indexes, run:
+
+-- EXPLAIN ANALYZE
+-- SELECT * FROM booking WHERE user_id = '9e9490ab-e46b-4565-ab3b-1231412051d8';
+
+-- -------------------------------------------------------------------
+-- Similarly, test other queries like joins or ordering by price_per_night:
+
+-- EXPLAIN ANALYZE
+-- SELECT p.property_id, p.name, p.location, p.price_per_night
+-- FROM properties p
+-- JOIN booking b ON p.property_id = b.property_id
+-- WHERE b.user_id = '9e9490ab-e46b-4565-ab3b-1231412051d8'
+-- ORDER BY p.price_per_night ASC;
+
+
